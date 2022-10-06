25475c1d580156329a72586c9fa9745655f04e91
yannick-cousin1 <yannick.cousin@laplateforme.io>
2022-10-06 16:54:49 +0200

    add backup script

--- backupFTP.sh
+++ backupFTP.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+yes | apt install lftp
+
+
 mkdir -p ~/Backup/
-mkdir -p ~/Backup/conf
-mkdir -p ~/Backup/data
+mkdir -p ~/conf
+mkdir -p ~/data
 
-cp /etc/proftpd/proftpd.conf ~/Backup/conf/
-cp /etc/proftpd/tls.conf ~/Backup/conf/
-cp /etc/proftpd/modules.conf ~/Backup/conf/
+cp /etc/proftpd/proftpd.conf ~/conf/
+cp /etc/proftpd/tls.conf ~/conf/
+cp /etc/proftpd/modules.conf ~/conf/
 
-cp -r /home/ ~/Backup/data/
+cp -r /home/ ~/data/
 
 d=$(date +"%e-%m-%Y-%H-%M")
+d=${d// /}
 comparchive="backup-$d.tar.gz"
-tar -zcf ~/Backup/$comparchive ~/Backup/conf/ ~/Backup/data
+tar -zcf ~/Backup/$comparchive ~/conf/ ~/data
+
+
+echo -e "set ssl:verify-certificate no\nput ~/Backup/$comparchive" | lftp merry:kalimac@10.10.39.137
+
+
 
 
